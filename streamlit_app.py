ce1fa2145f01e0a1be10673415eddc941fb22149
Amiiine <162085972+Amiiiiiiiiiiine@users.noreply.github.com>
2024-08-15 02:31:39 +0200

    Update streamlit_app.py
    deactivate get_active_session

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,7 @@ name_on_order = st.text_input("Name on Smoothie :")
 st.write("The name on your Smoothie will be : ", name_on_order)
 
 # Affichage de la table fruit_options
-session = get_active_session()
+#session = get_active_session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
